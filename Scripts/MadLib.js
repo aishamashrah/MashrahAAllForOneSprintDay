073f53d2a662b06d5e5182028a8040c16ac31f4d
aisha mashrah <aishamashrah@gmail.com>
2023-01-13 16:49:54 -0800

    Done

--- Scripts/MadLib.js
+++ Scripts/MadLib.js
@@ -1,7 +1,45 @@
-function MadLib(){
-    fetch('')
-      .then((response) => response.text())
-      .then((data) => console.log(data));
-    }
+// function MadLib(){
+//     fetch('')
+//       .then((response) => response.text())
+//       .then((data) => console.log(data));
+//     }
   
-  MadLib();
+//   MadLib();
+
+
+
+let Madlipinput1 = document.getElementById("Madlipinput1");
+let Madlipinput2 = document.getElementById("Madlipinput2");
+let Madlipinput3 = document.getElementById("Madlipinput3");
+let Madlipinput4 = document.getElementById("Madlipinput4");
+let Madlipinput5 = document.getElementById("Madlipinput5");
+let Madlipinput6 = document.getElementById("Madlipinput6");
+let Madlipinput7 = document.getElementById("Madlipinput7");
+let Madlipinput8 = document.getElementById("Madlipinput8");
+let Madlipinput9 = document.getElementById("Madlipinput9");
+let Madlipinput10 = document.getElementById("Madlipinput10");
+
+let madlipReturned = document.getElementById("madlipReturned");
+let madlipBtn = document.getElementById("madlipBtn");
+
+
+madlipBtn.addEventListener("click", function () {
+  GetData()
+})
+
+function GetData() {
+  fetch("https://aishaallforoneapi.azurewebsites.net/NewApi/Mad/" 
+  + Madlipinput1.value + 
+  "/" + Madlipinput2.value +
+   "/" + Madlipinput3.value + 
+   "/" + Madlipinput4.value + 
+   "/" + Madlipinput5.value + 
+   "/" + Madlipinput6.value + 
+   "/" + Madlipinput7.value + 
+   "/" + Madlipinput8.value + 
+   "/" + Madlipinput9.value + 
+   "/" + Madlipinput10.value).then(
+    response => response.text()).then(
+      data => madlipReturned.textContent = data
+    )
+}
--- Scripts/StudentDirectory.js
+++ Scripts/StudentDirectory.js
@@ -1,9 +1,32 @@
 
 
-function Student(){
-    fetch('https://aishaallforoneapi.azurewebsites.net/NewApi/GetStudentByFirstName/Aisha')
-      .then((response) => response.text())
-      .then((data) => console.log(data));
-    }
+// function Student(){
+//     fetch('https://aishaallforoneapi.azurewebsites.net/NewApi/GetStudentByFirstName/Aisha')
+//       .then((response) => response.text())
+//       .then((data) => console.log(data));
+//     }
   
-Student();
+// Student();
+
+
+let studentinput = document.getElementById("studentinput");
+let StudentBtn = document.getElementById("StudentBtn");
+//
+let studentsreturn = document.getElementById("studentsreturn");
+
+
+
+
+StudentBtn.addEventListener("click", function () {
+  GetData()
+ 
+})
+
+function GetData() {
+  fetch("https://aishaallforoneapi.azurewebsites.net/NewApi/GetStudentByFirstName/" + studentinput.value ).then(
+    response => response.json()).then(
+      data => {studentsreturn.textContent = "Student first Name : " + data.firstName + "Student last Name : " +  data.lastName + data.slackName
+      }
+    )
+}
+
